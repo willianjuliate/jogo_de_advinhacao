8f35334496471f1b26cf5908b32407d597be85e4
Willian <willianjuliate@gmail.com>
2021-02-22 09:13:30 -0300

    programa vai pedir a primeira entrada do usuário, e vai conferir se está no formto esperado.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Adivinhe o número!");
+    println!("Digite seu Palpite.");
+
+    let mut palpite = String::new();
+    io::stdin()
+        .read_line(&mut palpite)
+        .expect("Error! Falha ou ler a entrada");
+    println!("Voçê disse: {}", palpite);
 }
