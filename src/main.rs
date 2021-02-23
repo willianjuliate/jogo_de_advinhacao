19a42a50f2e643f76756a105277e5184374ed353
Willian <willianjuliate@gmail.com>
2021-02-23 15:37:55 -0300

    Adicionado novas expressoes 'loop' e 'match' e jogo finalizado!

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,33 @@
-extern crate rand; // Essa linha já não e mais necessária!
-
 use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
+use std::process::Command;
 
 fn main() {
-    println!("Adivinhe o número!");
+    println!("### ADIVINHE O NÚMERO! ###");
+    let numero_secreto = rand::thread_rng().gen_range(1..=100); //.gen_range(1..101) -> 0.8.3  .gen_range(1, 101) -> 0.5.5
+    loop {
+        let mut palpite = String::new();
 
-    let numero_secreto: i32 = rand::thread_rng().gen_range(1..=100); //.gen_range(1..101) -> 0.8.3  .gen_range(1, 101) -> 0.5.5
-    let mut palpite = String::new();
+        println!("Digite seu Palpite.: ");
 
-    println!("O número secreto é: {}", numero_secreto);
-    println!("Digite seu Palpite.");
+        io::stdin() //Biblioteca padrao do rust de entrada (input)
+            .read_line(&mut palpite) // Entrada padrão do teclado
+            .expect("Error! Falha ou ler a entrada"); // Enum que retorna uma execao casa uma condicão seja atendida.
+        let palpite: u32 = match palpite.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("Voçê disse: {}", palpite);
 
-    io::stdin() //Biblioteca padrao do rust de entrada (input)
-        .read_line(&mut palpite) // Entrada padrão do teclado
-        .expect("Error! Falha ou ler a entrada"); // Enum que retorna uma execao casa uma condicão seja atendida.
-    let palpite: i32 = palpite
-        .trim()
-        .parse()
-        .expect("Por favor, digite um número valido!");
-
-    println!("Voçê disse: {}", palpite);
-
-    match palpite.cmp(&numero_secreto) {
-        Ordering::Less => println!("Muito Baixo!"),
-        Ordering::Greater => println!("Muito Alto!"),
-        Ordering::Equal => println!("Você acertou!"),
+        match palpite.cmp(&numero_secreto) {
+            Ordering::Less => println!("Muito Baixo!"),
+            Ordering::Greater => println!("Muito Alto!"),
+            Ordering::Equal => {
+                println!("Você acertou!");
+                break;
+            }
+        }
     }
+    let _ = Command::new("cmd.exe").arg("/c").arg("pause").status();
 }
